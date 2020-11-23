7c9a9df2a2e365abd1f5140403c02a59a35d7a3f
Pierre Penhard <p.penhard@gmail.com>
2020-11-23 15:51:32 +0100

    Fixed migrations

--- db/migrate/20201123132236_devise_create_users.rb
+++ db/migrate/20201123132236_devise_create_users.rb
@@ -5,9 +5,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
-      t.string :username,           null: false, default: ""
-      t.string :first_name,         null: false, default: ""
-      t.string :last_name,          null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_135038) do
+ActiveRecord::Schema.define(version: 2020_11_23_143742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,9 @@ ActiveRecord::Schema.define(version: 2020_11_23_135038) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username", default: ""
+    t.string "first_name", default: ""
+    t.string "last_name", default: ""
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
