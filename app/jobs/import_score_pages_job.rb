110452db75a1f6d28703c042c3fc4ffebc56d5ca
gael gendre <gael.gendre@gmail.com>
2020-11-25 17:55:17 +0100

    all green

--- app/jobs/import_score_pages_job.rb
+++ app/jobs/import_score_pages_job.rb
@@ -4,11 +4,13 @@ class ImportScorePagesJob < ApplicationJob
   queue_as :default
 
   def perform(score)
-    score.page_count.times do |i|
-      url = Cloudinary::Utils.cloudinary_url(score.file.key, page: i + 1)
-      page = Page.new(score: score)
-      page.file.attach(io: open("#{url}.jpg"), filename: 'file')
-      page.save
-    end
+    # score.page_count.times do |i|
+    #   url = Cloudinary::Utils.cloudinary_url(score.file.key, page: i + 1)
+    #   page = Page.new(score: score)
+    #   page.file.attach(io: open("#{url}.jpg"), filename: 'file')
+    #   page.save
+    # end
+    puts score.file.key
+    puts "🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🧨🎠"
   end
 end
