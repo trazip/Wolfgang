fafb9ecae7fffb905c8d8cdfbc96f328b89664b1
gael gendre <gael.gendre@gmail.com>
2020-11-25 10:52:41 +0100

    pictures_and_dates

--- app/models/composer.rb
+++ app/models/composer.rb
@@ -1,3 +1,4 @@
 class Composer < ApplicationRecord
   has_many :scores
+  has_one_attached :picture
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@ nathalie_first_collection = Collection.new(private: false)
 nathalie_first_collection.user = nathalie
 nathalie_first_collection.save!
 
-puts "Birth of Wolfgang Amadeus Mozart 👼"
+puts "Birth of Wolfgang Amadeus Mozart 👼 (1756 – 1791)"
 mozart = Composer.new(name: "Wolfgang Amadeus Mozart")
 file = URI.open(Dir[File.join(File.dirname(__FILE__), "../app/assets/images/composers/wolfgang_amadeus_mozart.jpg")].first)
 mozart.picture.attach(io: file, filename: 'wolfgang_amadeus_mozart.jpg', content_type: 'image/jpg')
@@ -43,7 +43,7 @@ befraget_mich_ein_zartes_kind.composer = mozart
 befraget_mich_ein_zartes_kind.collection = pierre_first_collection
 befraget_mich_ein_zartes_kind.save!
 
-puts "Birth of Giuseppe Verdi 👼"
+puts "Birth of Giuseppe Verdi 👼 (1813 – 1901)"
 verdi = Composer.new(name: "Giuseppe Verdi")
 file = URI.open(Dir[File.join(File.dirname(__FILE__), "../app/assets/images/composers/giuseppe_verdi.jpg")].first)
 verdi.picture.attach(io: file, filename: 'giuseppe_verdi.jpg', content_type: 'image/jpg')
@@ -55,7 +55,7 @@ che_non_avrebbe_il_misero.composer = verdi
 che_non_avrebbe_il_misero.collection = pierre_first_collection
 che_non_avrebbe_il_misero.save!
 
-puts "Birth of Justin Hurwitz 👼"
+puts "Birth of Justin Hurwitz 👼 (1985 - )"
 hurwitz = Composer.new(name: "Justin Hurwitz")
 file = URI.open(Dir[File.join(File.dirname(__FILE__), "../app/assets/images/composers/justin_hurwitz.jpg")].first)
 hurwitz.picture.attach(io: file, filename: 'justin_hurwitz.jpg', content_type: 'image/jpg')
@@ -67,7 +67,7 @@ mia_and_Sebastians_theme.composer = hurwitz
 mia_and_Sebastians_theme.collection = pierre_first_collection
 mia_and_Sebastians_theme.save!
 
-puts "Birth of Ella Fitzgerald 👼"
+puts "Birth of Ella Fitzgerald 👼 (1917 – 1996)"
 fitzgerald = Composer.new(name: "Ella Fitzgerald")
 file = URI.open(Dir[File.join(File.dirname(__FILE__), "../app/assets/images/composers/ella_fitzgerald.jpg")].first)
 fitzgerald.picture.attach(io: file, filename: 'ella_fitzgerald.jpg', content_type: 'image/jpg')
@@ -79,7 +79,7 @@ all_of_me.collection = pierre_first_collection
 all_of_me.composer = fitzgerald
 all_of_me.save!
 
-puts "Creation of The Cinematic Orchestra 👼"
+puts "Creation of The Cinematic Orchestra 🎉 (1999 - )"
 cinematic_orchestra = Composer.new(name: "The Cinematic Orchestra")
 file = URI.open(Dir[File.join(File.dirname(__FILE__), "../app/assets/images/composers/cinematic_orchestra.jpg")].first)
 cinematic_orchestra.picture.attach(io: file, filename: 'cinematic_orchestra.jpg', content_type: 'image/jpg')
@@ -91,7 +91,7 @@ arrival_of_the_birds.composer = cinematic_orchestra
 arrival_of_the_birds.collection = pierre_first_collection
 arrival_of_the_birds.save!
 
-puts "Birth of Erik Satie 👼"
+puts "Birth of Erik Satie 👼 (1866 – 1925)"
 satie = Composer.new(name: "Erik Satie")
 file = URI.open(Dir[File.join(File.dirname(__FILE__), "../app/assets/images/composers/erik_satie.jpg")].first)
 satie.picture.attach(io: file, filename: 'erik_satie.jpg', content_type: 'image/jpg')
@@ -103,7 +103,7 @@ gymnopedie_no_1.composer = satie
 gymnopedie_no_1.collection = nathalie_first_collection
 gymnopedie_no_1.save!
 
-puts "Birth of Ennio Morricone 👼"
+puts "Birth of Ennio Morricone 👼 (1928 – 2020)"
 morricone = Composer.new(name: "Ennio Morricone")
 file = URI.open(Dir[File.join(File.dirname(__FILE__), "../app/assets/images/composers/ennio_morricone.jpg")].first)
 morricone.picture.attach(io: file, filename: 'ennio_morricone.jpg', content_type: 'image/jpg')
@@ -115,7 +115,7 @@ gabriels_oboe.composer = morricone
 gabriels_oboe.collection = nathalie_first_collection
 gabriels_oboe.save!
 
-puts "Birth of James Brown 👼"
+puts "Birth of James Brown 👼 (1933 – 2006)"
 brown = Composer.new(name: "James Brown")
 file = URI.open(Dir[File.join(File.dirname(__FILE__), "../app/assets/images/composers/james_brown.jpg")].first)
 brown.picture.attach(io: file, filename: 'james_brown.jpg', content_type: 'image/jpg')
@@ -127,7 +127,7 @@ i_feel_good.composer = brown
 i_feel_good.collection = nathalie_first_collection
 i_feel_good.save!
 
-puts "Birth of Franz Schubert 👼"
+puts "Birth of Franz Schubert 👼 (1797 – 1828)"
 schubert = Composer.new(name: "Franz Schubert")
 file = URI.open(Dir[File.join(File.dirname(__FILE__), "../app/assets/images/composers/franz_schubert.jpg")].first)
 schubert.picture.attach(io: file, filename: 'franz_schubert.jpg', content_type: 'image/jpg')
@@ -139,7 +139,7 @@ standchen_from_Schwanengesang_Serenade_for_Piano.composer = schubert
 standchen_from_Schwanengesang_Serenade_for_Piano.collection = nathalie_first_collection
 standchen_from_Schwanengesang_Serenade_for_Piano.save!
 
-puts "Birth of Hans Zimmer 👼"
+puts "Birth of Hans Zimmer 👼 (1957 - )"
 zimmer = Composer.new(name: "Hans Zimmer")
 file = URI.open(Dir[File.join(File.dirname(__FILE__), "../app/assets/images/composers/hans_zimmer.jpg")].first)
 zimmer.picture.attach(io: file, filename: 'hans_zimmer.jpg', content_type: 'image/jpg')
