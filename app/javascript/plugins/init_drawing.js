92b7c4f9f1b0d47023480099be87c70a42b41b0b
Pierre Penhard <p.penhard@gmail.com>
2020-11-26 17:41:46 +0100

    Added style to show score

--- app/javascript/plugins/init_drawing.js
+++ app/javascript/plugins/init_drawing.js
@@ -23,50 +23,52 @@ const submitData = (sdb, pageId) => {
 
 const initDrawing = () => {
   const zone = document.querySelector('#zone');
-  initCanvas(zone);
+  if (zone) {
+    initCanvas(zone);
 
-  const undo = document.querySelector('#undo');
-  const clear = document.querySelector('#clear');
-  const mode = document.querySelector('#mode');
-  const canvas = document.querySelector('#canvas');
-  const viewer = document.querySelector('#viewer');
-  const pageId = viewer.dataset.pageId;
-  const annotation = viewer.dataset.annotation;
-  const sdb = create(canvas);
-  sdb.setLineSize(2);
-  sdb.setLineColor("#fc2525");
-  //sdb.toggleMode();
+    const undo = document.querySelector('#undo');
+    const clear = document.querySelector('#clear');
+    const mode = document.querySelector('#mode');
+    const canvas = document.querySelector('#canvas');
+    const viewer = document.querySelector('#viewer');
+    const pageId = viewer.dataset.pageId;
+    const annotation = viewer.dataset.annotation;
+    const sdb = create(canvas);
+    sdb.setLineSize(2);
+    sdb.setLineColor("#fc2525");
+    //sdb.toggleMode();
 
-  if (annotation !== ''){
-    sdb.fillImageByDataURL(annotation);
-  }
-
-
-
-  sdb.observer.on('drawEnd', e => {
-    submitData(sdb, pageId);
-  });
-
-  mode.addEventListener('click', e => {
-    sdb.toggleMode();
-    if (mode.innerHTML === '<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>') {
-      mode.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg>';
-       sdb.setLineSize(2);
-    } else {
-      mode.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>';
-      sdb.setLineSize(30);
+    if (annotation !== ''){
+      sdb.fillImageByDataURL(annotation);
     }
-  })
 
-  clear.addEventListener('click', e => {
-    sdb.clear();
-    submitData(sdb, pageId);
-  })
 
-  undo.addEventListener('click', async (e) => {
-    await sdb.undo();
-    submitData(sdb, pageId);
-  })
+
+    sdb.observer.on('drawEnd', e => {
+      submitData(sdb, pageId);
+    });
+
+    mode.addEventListener('click', e => {
+      sdb.toggleMode();
+      if (mode.innerHTML === '<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>') {
+        mode.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg>';
+         sdb.setLineSize(2);
+      } else {
+        mode.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>';
+        sdb.setLineSize(30);
+      }
+    })
+
+    clear.addEventListener('click', e => {
+      sdb.clear();
+      submitData(sdb, pageId);
+    })
+
+    undo.addEventListener('click', async (e) => {
+      await sdb.undo();
+      submitData(sdb, pageId);
+    })
+  }
 }
 
 export {initDrawing}
