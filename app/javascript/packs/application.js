441d3f0f3ad6769f66f6365871929c3ebb2684a7
Bastien Duban <duban.bastien@gmail.com>
2020-11-27 12:36:11 +0100

    selectr done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,10 +30,14 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initDrawing } from '../plugins/init_drawing';
 import { initSwiper} from '../plugins/init_swiper';
+import Selectr from "mobius1-selectr"
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   initSwiper();
   initDrawing();
+
+  new Selectr(document.getElementById("selectr"))
+
   // initSelect2();
 });
