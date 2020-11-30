c740a2587bda18b2039157d9ccf5788109895903
Pierre Penhard <p.penhard@gmail.com>
2020-11-30 17:50:11 +0100

    Added style to icons

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,4 +2,20 @@ class Page < ApplicationRecord
   belongs_to :score
   has_one :annotation, dependent: :destroy
   has_one_attached :file
+
+  def is_first?
+    score.pages.first == self
+  end
+
+  def is_last?
+    score.pages.last == self
+  end
+
+  def next
+    score.pages[score.pages.find_index(self) + 1]
+  end
+
+  def previous
+    score.pages[score.pages.find_index(self) - 1]
+  end
 end
